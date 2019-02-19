68d2b74f0d33a08859a4be458f8ba969a48482e4
zyz51325 <1287318179@qq.com>
2019-02-19 11:29:42 +0800

    一起出去玩

---
+++ ak47/ak47/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ak47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            //2222222222222222222222222222222222222222222222222222
+        }
+    }
+}
